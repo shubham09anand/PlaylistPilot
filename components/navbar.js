96fc978336f21296f8046222a77f6f71605068d8
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-02-22 17:49:48 +0530

    initiated frontend

--- components/navbar.js
+++ components/navbar.js
@@ -24,8 +24,8 @@ const Navbar = () => {
       <div className="navbar flex flex-col md:flex-row items-center bg-gray-800 md:p-2 shadow-lg py-4 ">
      <div className=' top-0 left-0 absolute m-7 md:m-10 text-3xl font-thin md:hidden'> <GiHamburgerMenu onClick={toggle}  /></div>
         <Link href={"/"}><div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 md:z-10 my-4 md:my-0 mx-10 cursor-pointer">
-          {/* <img src="" alt="" /> */}
-          <span className="ml-3 text-xl text-white">PlayListPilot</span>
+          <img src="logo.svg" alt="" />
+          {/* <span className="ml-3 text-xl text-white">PlayListPilot</span> */}
         </div></Link>
         <div ref={ref} className="navitems flex flex-col md:flex-row md:items-center md:bg-inherit bg-yellow-400 px-10 py-4 md:py-0 absolute top-0 left-0 transform transition-transform -translate-x-full h-full md:h-auto md:w-full w-2/3 md:static md:translate-x-0  md:transition-none ease-in-out" >
           <span className='absolute top-8 left-8
--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='min-h-screen'>
-      this is next app
-     
+    <div className='min-h-screen bg-gray-800 flex flex-col py-40 items-center space-y-4'>
+      <div className="flex  font-bold justify-center  text-4xl  z-20 text-white">
+      Satisfy Your OCD
+      </div>
+      <p className='text-2xl text-white'>Load up, filter out and organize your playlists.</p>
+      <button class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded-full text-lg translate-y-10 ">LETS GO!</button>     
     </div>
   )
 }
